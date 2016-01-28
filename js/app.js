e236233f051e00f2eff5a4648f293141e031719c
Lissa Walzer <lissawalzer@gmail.com>
2016-01-28 15:18:52 -0800

    Added a route

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp',['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider){
     $routeProvider
-        .when('/', {
-            templateUrl: 'partials/search.html',
-            controller: 'searchController'
-        })
-})
+      .when('/', {
+        templateUrl: 'partials/search.html',
+        controller: 'searchController'
+      })
+      $locationProvider.html5Mode(true);
+});
 
-app.controller('searchController', function($scope) {
-    $scope.test = "This is a test";
-})
+app.controller("searchController", function($scope){
+  $scope.message = "It worked bitches";
+});
 
 app.controller('testController', function($scope) {
     $scope.test = "This is a super test";
